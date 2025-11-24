ed9ede0afbab9e068ed07a4076724275ea755523
vijay kumar <vijay.127163@marwadiuniversity.ac.in>
2025-11-24 22:22:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function append(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+    let current = document.getElementById("display").value;
+    document.getElementById("display").value = current.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+    } catch {
+        document.getElementById("display").value = "Error";
+    }
+}
